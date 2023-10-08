796dc4770d17e4ebd10619eee571dd9ba89e8306
tihmihvik <tihmihvik@gmail.com>
2023-10-08 16:01:56 +0700

    Completing with Instagram Layout

--- app/src/main/java/ru/tihonov/firstcomposeproject/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/ru/tihonov/firstcomposeproject/ui/theme/InstagramProfileCard.kt
@@ -4,13 +4,16 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
@@ -31,22 +34,46 @@ fun InstagramProfileCard() {
         ),
         border = BorderStroke(1.dp, MaterialTheme.colors.onBackground)
     ) {
-        Row(
+Column(
+    modifier = Modifier.padding(16.dp)
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_instagram),
+            contentDescription = "Логотип Инстаграм",
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Image(painter = painterResource(id = R.drawable.ic_instagram),
-                contentDescription = "Логотип Инстаграм",
-                modifier = Modifier.size(50.dp)
+                .size(60.dp)
+                .clip(CircleShape)
+                .background(color = Color.White)
+                .padding(8.dp)
 
-            )
-            UserStatistics(title = "Posts", value = "6,950")
-            UserStatistics(title = "Followers", value = "436M")
-            UserStatistics(title = "Following", value = "76")
-        }
+        )
+        UserStatistics(title = "Posts", value = "6,950")
+        UserStatistics(title = "Followers", value = "436M")
+        UserStatistics(title = "Following", value = "76")
+    }
+    Text(
+        text = "Instagram",
+        fontSize = 32.sp,
+        fontFamily = FontFamily.Cursive
+    )
+    Text(
+        text = "№Вы подписались?",
+        fontSize = 14.sp
+    )
+    Text(
+        text = "www.facebook.com/",
+        fontSize = 14.sp
+    )
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = "Follow")
+    }
+}
     }
 }
 
