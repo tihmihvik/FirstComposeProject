fb9a2123b3bf62482be8d9342525c23dda07a72b
tihmihvik <tihmihvik@gmail.com>
2023-10-15 12:06:35 +0700

    Подключили кнопку. Добавили зависимость runtime-livedata

--- app/src/main/java/ru/tihonov/firstcomposeproject/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/ru/tihonov/firstcomposeproject/ui/theme/InstagramProfileCard.kt
@@ -6,11 +6,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -18,13 +19,17 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import ru.tihonov.firstcomposeproject.MainViewModel
 import ru.tihonov.firstcomposeproject.R
 
 @Composable
-fun InstagramProfileCard() {
+fun InstagramProfileCard(
+    viewModel: MainViewModel
+) {
+    val isFollowed = viewModel.isFollowing.observeAsState(false)
+
     Card(
         modifier = Modifier.padding(8.dp),
         backgroundColor = MaterialTheme.colors.background,
@@ -34,46 +39,68 @@ fun InstagramProfileCard() {
         ),
         border = BorderStroke(1.dp, MaterialTheme.colors.onBackground)
     ) {
-Column(
-    modifier = Modifier.padding(16.dp)
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_instagram),
-            contentDescription = "Логотип Инстаграм",
-            modifier = Modifier
-                .size(60.dp)
-                .clip(CircleShape)
-                .background(color = Color.White)
-                .padding(8.dp)
-
-        )
-        UserStatistics(title = "Posts", value = "6,950")
-        UserStatistics(title = "Followers", value = "436M")
-        UserStatistics(title = "Following", value = "76")
-    }
-    Text(
-        text = "Instagram",
-        fontSize = 32.sp,
-        fontFamily = FontFamily.Cursive
-    )
-    Text(
-        text = "№Вы подписались?",
-        fontSize = 14.sp
-    )
-    Text(
-        text = "www.facebook.com/",
-        fontSize = 14.sp
-    )
-    Button(onClick = { /*TODO*/ }) {
-        Text(text = "Follow")
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_instagram),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(60.dp)
+                        .clip(CircleShape)
+                        .background(Color.White)
+                        .padding(8.dp),
+                )
+                UserStatistics(title = "Posts", value = "6,950")
+                UserStatistics(title = "Followers", value = "436M")
+                UserStatistics(title = "Following", value = "76")
+            }
+            Text(
+                text = "Instagram",
+                fontSize = 32.sp,
+                fontFamily = FontFamily.Cursive
+            )
+            Text(
+                text = "#YoursToMake",
+                fontSize = 14.sp
+            )
+            Text(
+                text = "www.facebook.com/emotional_health",
+                fontSize = 14.sp
+            )
+            FollowButton(isFollowed = isFollowed.value) {
+                viewModel.changeFollowingStatus()
+            }
+        }
     }
 }
+
+@Composable
+private fun FollowButton(
+    isFollowed: Boolean,
+    clickListener: () -> Unit
+) {
+    Button(
+        onClick = { clickListener() },
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = if (isFollowed) {
+                MaterialTheme.colors.primary.copy(alpha = 0.5f)
+            } else {
+                MaterialTheme.colors.primary
+            }
+        )
+    ) {
+        val text = if (isFollowed) {
+            "Unfollow"
+        } else {
+            "Follow"
+        }
+        Text(text = text)
     }
 }
 
@@ -96,25 +123,5 @@ private fun UserStatistics(
             text = title,
             fontWeight = FontWeight.Bold
         )
-    }
-}
-
-@Preview
-@Composable
-fun PreviewCardLight() {
-    FirstComposeProjectTheme(
-        darkTheme = false
-    ) {
-        InstagramProfileCard()
-    }
-}
-
-@Preview
-@Composable
-fun PreviewCardDark() {
-    FirstComposeProjectTheme(
-        darkTheme = true
-    ) {
-        InstagramProfileCard()
     }
 }
