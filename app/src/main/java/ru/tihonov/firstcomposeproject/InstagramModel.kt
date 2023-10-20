be29f7c218bbf5f971ca48edf72002313b8df166
tihmihvik <tihmihvik@gmail.com>
2023-10-20 22:03:22 +0700

    Доработка. Добавили data class InstagramModel

---
+++ app/src/main/java/ru/tihonov/firstcomposeproject/InstagramModel.kt
@@ -0,0 +1,7 @@
+package ru.tihonov.firstcomposeproject
+
+data class InstagramModel(
+    val id: Int,
+    val title: String,
+    val isFollowed: Boolean
+)
--- app/src/main/java/ru/tihonov/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/ru/tihonov/firstcomposeproject/MainActivity.kt
@@ -3,17 +3,16 @@ package ru.tihonov.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.lifecycle.ViewModelProvider
 import ru.tihonov.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import ru.tihonov.firstcomposeproject.ui.theme.InstagramProfileCard
@@ -29,6 +28,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun Test(viewModel: MainViewModel) {
     FirstComposeProjectTheme {
@@ -37,18 +37,15 @@ private fun Test(viewModel: MainViewModel) {
                 .fillMaxSize()
                 .background(MaterialTheme.colors.background)
         ) {
+            val models = viewModel.models.observeAsState(listOf())
             LazyColumn {
-                item {
-                    Text(text = "Title", color = Color.White)
-                }
-                items(10) {
-                    InstagramProfileCard(viewModel)
-                }
-                item {
-                    Image(painter = painterResource(id = R.drawable.ic_instagram), contentDescription = null)
-                }
-                items(500) {
-                    InstagramProfileCard(viewModel)
+                items(models.value) { model ->
+                    InstagramProfileCard(
+                        model = model,
+                        onFollowedButtonClickListener = {
+                            viewModel.changeFollowingStatus(it)
+                        }
+                    )
                 }
             }
         }
