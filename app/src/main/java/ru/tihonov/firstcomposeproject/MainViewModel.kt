4235447f254f54ab909717e53b5287f4eb5b5ce1
tihmihvik <tihmihvik@gmail.com>
2023-10-21 13:26:06 +0700

    Добавлена навигация

--- app/src/main/java/ru/tihonov/firstcomposeproject/MainViewModel.kt
+++ app/src/main/java/ru/tihonov/firstcomposeproject/MainViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.tihonov.firstcomposeproject.domain.FeedPost
 import ru.tihonov.firstcomposeproject.domain.StatisticItem
+import ru.tihonov.firstcomposeproject.ui.theme.NavigationItem
 
 class MainViewModel : ViewModel() {
 
@@ -15,6 +16,13 @@ class MainViewModel : ViewModel() {
     }
     private val _feedPosts = MutableLiveData<List<FeedPost>>(sourceList)
     val feedPosts: LiveData<List<FeedPost>> = _feedPosts
+
+    private val _selectedNavItem = MutableLiveData<NavigationItem>(NavigationItem.Home)
+    val selectedNavItem: LiveData<NavigationItem> = _selectedNavItem
+
+    fun selectNavItem(item: NavigationItem) {
+        _selectedNavItem.value = item
+    }
 
     fun updateCount(feedPost: FeedPost, item: StatisticItem) {
         val oldPosts = feedPosts.value?.toMutableList() ?: mutableListOf()
