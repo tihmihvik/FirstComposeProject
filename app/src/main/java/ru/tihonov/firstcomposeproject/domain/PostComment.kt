7c6e6519f30c97b4aa5cece5183aae7686b63456
tihmihvik <tihmihvik@gmail.com>
2023-10-22 15:53:31 +0700

    Работа с комментариями

---
+++ app/src/main/java/ru/tihonov/firstcomposeproject/domain/PostComment.kt
@@ -0,0 +1,11 @@
+package ru.tihonov.firstcomposeproject.domain
+
+import ru.tihonov.firstcomposeproject.R
+
+data class PostComment(
+    val id: Int,
+    val authorName: String = "Author",
+    val authorAvatarId: Int = R.drawable.comment_author_avatar,
+    val commentText: String = "Long comment text",
+    val publicationDate: String = "14:00"
+)
